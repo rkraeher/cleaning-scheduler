9bb0219ff2132b496daa35aa516a956c95abf224
ru <rudikraeher@gmail.com>
2023-07-25 23:13:53 +0200

    refactor; renaming, moving vars, and reducing number of params for updateCleaner helper fn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// we loop through entire workbook, separating 'availables' from till '{{date}}'
-export const CLEANING_TIME_IN_MINUTES = {
+// we loop through entire workbook, distinguishing 'availables' from till '{{date}}'
+export const CLEANING_TIMES_IN_MINUTES = {
   D: 30,
   Q: 60,
   O: 120,
@@ -7,7 +7,6 @@ export const CLEANING_TIME_IN_MINUTES = {
 
 // this data will come from the spreadsheet
 export const availableRooms = {
-  // room# is one column, room key is the next
   101: 'DBS',
   102: 'QDB',
   103: 'QDS',
@@ -27,19 +26,16 @@ const occupiedRooms = {
 };
 
 const roomsMap = setRoomsMap(availableRooms);
-
-const totalCleaningTime = calculateTotalCleaningTime(
-  roomsMap,
-  CLEANING_TIME_IN_MINUTES
-);
-
 const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
 
 export function setRoomsMap(availableRooms) {
   let roomsMap = new Map();
 
-  for (const [room, cleaningTimeCode] of Object.entries(availableRooms)) {
-    roomsMap.set(room, cleaningTimeCode[0]);
+  for (const [roomNumber, cleaningTimeCodes] of Object.entries(
+    availableRooms
+  )) {
+    // in the user's system, only the first letter from the codes column is relevant
+    roomsMap.set(roomNumber, cleaningTimeCodes[0]);
   }
 
   return roomsMap;
@@ -47,13 +43,17 @@ export function setRoomsMap(availableRooms) {
 
 export function calculateTotalCleaningTime(roomsMap, CLEANING_TIMES) {
   let totalCleaningTime = 0;
-  for (const cleaningTimeCode of roomsMap.values()) {
-    totalCleaningTime += CLEANING_TIMES[cleaningTimeCode];
+  for (const cleaningTime of roomsMap.values()) {
+    totalCleaningTime += CLEANING_TIMES[cleaningTime];
   }
   return totalCleaningTime;
 }
 
 export function getRoomAssignments(roomsMap) {
+  const totalCleaningTime = calculateTotalCleaningTime(
+    roomsMap,
+    CLEANING_TIMES_IN_MINUTES
+  );
   const TOTAL_CLEANING_TIME_FOR_SINGLE_CLEANER =
     'totalCleaningTimeForSingleCleaner';
 
@@ -63,17 +63,19 @@ export function getRoomAssignments(roomsMap) {
   let cleanerB = new Map();
   cleanerB.set(TOTAL_CLEANING_TIME_FOR_SINGLE_CLEANER, 0);
 
-  function updateCleaner(cleaner, room, cleaningTimeForOneRoom) {
-    cleaner.set(room, cleaningTimeForOneRoom);
+  function updateCleaner(cleaner, room) {
+    const [roomNumber, cleaningTime] = room;
+
+    cleaner.set(roomNumber, cleaningTime);
     cleaner.set(
       TOTAL_CLEANING_TIME_FOR_SINGLE_CLEANER,
-      cleaner.get(TOTAL_CLEANING_TIME_FOR_SINGLE_CLEANER) +
-        cleaningTimeForOneRoom
+      cleaner.get(TOTAL_CLEANING_TIME_FOR_SINGLE_CLEANER) + cleaningTime
     );
   }
 
-  for (const [room, cleaningTimeCode] of roomsMap) {
-    const cleaningTimeForOneRoom = CLEANING_TIME_IN_MINUTES[cleaningTimeCode];
+  for (const [roomNumber, cleaningTimeCode] of roomsMap) {
+    const cleaningTimeForOneRoom = CLEANING_TIMES_IN_MINUTES[cleaningTimeCode];
+    const room = [roomNumber, cleaningTimeForOneRoom];
 
     const IS_CLEANING_TIME_FIT_FOR_SINGLE_CLEANER =
       cleanerA.get(TOTAL_CLEANING_TIME_FOR_SINGLE_CLEANER) <
@@ -83,9 +85,9 @@ export function getRoomAssignments(roomsMap) {
         Math.floor(totalCleaningTime / 2);
 
     if (IS_CLEANING_TIME_FIT_FOR_SINGLE_CLEANER) {
-      updateCleaner(cleanerA, room, cleaningTimeForOneRoom);
-    } else if (!cleanerA.has(room)) {
-      updateCleaner(cleanerB, room, cleaningTimeForOneRoom);
+      updateCleaner(cleanerA, room);
+    } else if (!cleanerA.has(roomNumber)) {
+      updateCleaner(cleanerB, room);
     }
   }
   return [cleanerA, cleanerB];
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import {
-  CLEANING_TIME_IN_MINUTES,
+  CLEANING_TIMES_IN_MINUTES,
   availableRooms,
   setRoomsMap,
   calculateTotalCleaningTime,
@@ -10,7 +10,7 @@ describe('calculateTotalCleaningTime', () => {
   test('should calculate total cleaning time correctly', () => {
     const totalCleaningTime = calculateTotalCleaningTime(
       setRoomsMap(availableRooms),
-      CLEANING_TIME_IN_MINUTES
+      CLEANING_TIMES_IN_MINUTES
     );
 
     const expectedTotalCleaningTime = 750;
@@ -22,23 +22,25 @@ describe('calculateTotalCleaningTime', () => {
 });
 
 describe.skip('getRoomAssignments', () => {
+  // should divide the totalCleaningTime in half
+  // should assign rooms to each cleaner
+  // should handle an even-numbered totalCleaningTime
+  // should handle an odd-numbered totalCleaningTime
+  // if odd totalCleaningTime, never a difference > 30 min between the cleaners
   test('should evenly assign rooms to two cleaners according to the required cleaning times', () => {
     const roomsMap = setRoomsMap(availableRooms);
 
     const totalCleaningTime = calculateTotalCleaningTime(
       roomsMap,
-      CLEANING_TIME_IN_MINUTES
+      CLEANING_TIMES_IN_MINUTES
     );
 
     const [cleanerA, cleanerB] = getRoomAssignments(
       roomsMap,
-      CLEANING_TIME_IN_MINUTES,
+      CLEANING_TIMES_IN_MINUTES,
       totalCleaningTime
     );
 
     expect(cleanerA.get(101)).toBe(30);
   });
-
-  // splits in half
-  // if odd totalCleaningTime, never a difference > 30 min between both cleaners
 });
