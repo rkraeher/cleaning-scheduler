56570018a3065cd35bcca2c8f6aa3b0def13f5e8
ru <rudikraeher@gmail.com>
2023-07-27 15:48:36 +0200

    add more test cases, except for the minimizing time difference case

--- index.test.js
+++ index.test.js
@@ -20,7 +20,7 @@ const mockAvailableRooms = {
 };
 
 describe('calculateTotalCleaningTime', () => {
-  test('should calculate total cleaning time correctly', () => {
+  it('should calculate total cleaning time correctly', () => {
     const totalCleaningTime = calculateTotalCleaningTime(
       setRoomsMap(mockAvailableRooms)
     );
@@ -35,7 +35,7 @@ describe('calculateTotalCleaningTime', () => {
 });
 
 describe('getRoomAssignments', () => {
-  test('should distribute all the rooms between two cleaners', () => {
+  it('should distribute all the rooms between two cleaners', () => {
     const roomsMap = setRoomsMap(mockAvailableRooms);
     const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
 
@@ -56,6 +56,68 @@ describe('getRoomAssignments', () => {
     expect(
       allRooms.every((room) => cleanerA.has(room) || cleanerB.has(room))
     ).toBeTruthy();
+  });
+
+  it('should only assign a room to one cleaner', () => {
+    const roomsMap = setRoomsMap(mockAvailableRooms);
+    const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
+
+    const cleanerArooms = ['101', '102', '103', '203', '207', '211', '215'];
+    const cleanerBrooms = ['210', '212', '213', '214', '216'];
+
+    expect(cleanerArooms.every((room) => !cleanerB.has(room))).toBeTruthy();
+    expect(cleanerBrooms.every((room) => !cleanerA.has(room))).toBeTruthy();
+  });
+
+  it('should handle an even-numbered totalCleaningTime', () => {
+    const roomsWithEvenNumberedTotalCleaningTime = {
+      101: 'DBS',
+      102: 'QDB',
+      103: 'QDS',
+      203: 'OC1',
+      207: 'DDY',
+      210: 'ODY',
+      211: 'DBS',
+      212: 'QDB',
+      213: 'QDS',
+      214: 'OC1',
+      215: 'DDY',
+    };
+
+    const roomsMap = setRoomsMap(roomsWithEvenNumberedTotalCleaningTime);
+    const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
+    const evenNumberedTotalCleaningTime = calculateTotalCleaningTime(roomsMap);
+
+    expect(evenNumberedTotalCleaningTime % 2 == 0).toBeTruthy();
+    expect(cleanerA.get('totalCleaningTimeForOneCleaner')).toEqual(
+      cleanerB.get('totalCleaningTimeForOneCleaner')
+    );
+  });
+
+  it('should handle an odd-numbered totalCleaningTime', () => {
+    const roomsWithOddNumberedTotalCleaningTime = {
+      101: 'ABS',
+      102: 'QDB',
+      103: 'QDS',
+      203: 'OC1',
+      207: 'DDY',
+      210: 'ODY',
+      211: 'DBS',
+      212: 'QDB',
+      213: 'QDS',
+      214: 'OC1',
+      215: 'DDY',
+      216: 'DDY',
+    };
+
+    const roomsMap = setRoomsMap(roomsWithOddNumberedTotalCleaningTime);
+    const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
+    const oddNumberedTotalCleaningTime = calculateTotalCleaningTime(roomsMap);
+
+    expect(oddNumberedTotalCleaningTime % 2 == 1).toBeTruthy();
+    expect(cleanerA.get('totalCleaningTimeForOneCleaner')).not.toEqual(
+      cleanerB.get('totalCleaningTimeForOneCleaner')
+    );
   });
 
   // 2. should only assign a room to one cleaner (no duplicate room assignments)
