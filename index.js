adcced753e34e4f3c72add2a231c6c871f32872d
ru <rudikraeher@gmail.com>
2023-07-28 00:46:47 +0200

    improve algo to distribute the rooms with a minimal sum difference; with tests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // anything that is 'available' or till ...next day = departure ('Q','D', or 'O' cleaning time)
 // everything that has till ...2 days in future, = occupied (15 mins)
 const CLEANING_TIMES_IN_MINUTES = {
+  A: 15,
   D: 30,
   Q: 60,
   O: 120,
@@ -9,7 +10,7 @@ const CLEANING_TIMES_IN_MINUTES = {
 
 // this data will come from the spreadsheet
 const availableRooms = {
-  101: 'DBS',
+  101: 'ABS',
   102: 'QDB',
   103: 'QDS',
   203: 'OC1',
@@ -23,11 +24,9 @@ const availableRooms = {
   216: 'DDY',
 };
 
-const occupiedRooms = {
-  // we just need room numbers, because all durations for occupied rooms will be 15
-};
-
 const [cleanerA, cleanerB] = getRoomAssignments(setRoomsMap(availableRooms));
+const totalCleaningTimeCleanerA = sumTotalCleaningTime(cleanerA);
+const totalCleaningTimeCleanerB = sumTotalCleaningTime(cleanerB);
 
 export function setRoomsMap(availableRooms) {
   let roomsMap = new Map();
@@ -42,49 +41,106 @@ export function setRoomsMap(availableRooms) {
   return roomsMap;
 }
 
-export function calculateTotalCleaningTime(roomsMap) {
-  let totalCleaningTime = 0;
-  for (const cleaningTime of roomsMap.values()) {
-    totalCleaningTime += cleaningTime;
+export function sumTotalCleaningTime(cleaner) {
+  return [...cleaner.values()].reduce((sum, value) => sum + value, 0);
+}
+
+function getClosestSumDifference(roomsMap) {
+  const totalSum = sumTotalCleaningTime(roomsMap);
+
+  // Create a set to store all the possible sums of cleaning times
+  const possibleSums = new Set();
+
+  for (const [, cleaningTime] of roomsMap) {
+    const newPossibleSums = new Set();
+    for (const sum of possibleSums) {
+      newPossibleSums.add(sum + cleaningTime);
+    }
+    possibleSums.add(cleaningTime);
+    for (const sum of newPossibleSums) {
+      possibleSums.add(sum);
+    }
   }
-  return totalCleaningTime;
+
+  // Calculate the maximum sum that each cleaner should have
+  const exactlyEqualSum = totalSum / 2;
+  let closestSum = 0;
+
+  // Find the closest sum to the maximum sum that can be achieved
+  for (const sum of possibleSums) {
+    if (sum <= exactlyEqualSum && sum > closestSum) {
+      closestSum = sum;
+    }
+  }
+
+  return closestSum;
+}
+
+// EDIT ME
+function findRoomCombination(roomsMap, closestSum) {
+  // IS THIS NECESSARY?
+  const rooms = Array.from(roomsMap.entries()); // Convert the Map to an array of [roomNumber, cleaningTime] pairs
+  const roomCount = rooms.length;
+  let chosenRooms = [];
+
+  // Helper function to calculate the sum of cleaning times for the given combination of rooms
+  function calculateSum(combination) {
+    return combination.reduce((sum, [, cleaningTime]) => sum + cleaningTime, 0);
+  }
+
+  // Helper function to perform backtracking
+  function backtrack(start) {
+    const sum = calculateSum(chosenRooms);
+
+    // If the sum equals the closestSum, we found a valid combination
+    if (sum === closestSum) {
+      return { chosenRooms, closestSum };
+    }
+
+    // If the sum is greater than the closestSum or we have explored all rooms, stop
+    if (sum > closestSum || start === roomCount) {
+      return null;
+    }
+
+    // Explore all possible combinations
+    for (let i = start; i < roomCount; i++) {
+      chosenRooms.push(rooms[i]);
+      const result = backtrack(i + 1);
+      if (result) {
+        return result;
+      }
+      chosenRooms.pop();
+    }
+
+    return null;
+  }
+
+  // Start backtracking from index 0
+  return backtrack(0);
 }
 
 export function getRoomAssignments(roomsMap) {
-  const TOTAL_CLEANING_TIME_FOR_ONE_CLEANER = 'totalCleaningTimeForOneCleaner';
-  const totalCleaningTime = calculateTotalCleaningTime(roomsMap);
+  const { chosenRooms } = findRoomCombination(
+    roomsMap,
+    getClosestSumDifference(roomsMap)
+  );
 
-  let cleanerA = new Map([[TOTAL_CLEANING_TIME_FOR_ONE_CLEANER, 0]]);
-  let cleanerB = new Map([[TOTAL_CLEANING_TIME_FOR_ONE_CLEANER, 0]]);
+  let cleanerA = new Map(chosenRooms);
+  let cleanerB = new Map();
 
-  function updateCleaner(cleaner, room) {
-    const [roomNumber, cleaningTimeForOneRoom] = room;
-
-    cleaner.set(roomNumber, cleaningTimeForOneRoom);
-    cleaner.set(
-      TOTAL_CLEANING_TIME_FOR_ONE_CLEANER,
-      cleaner.get(TOTAL_CLEANING_TIME_FOR_ONE_CLEANER) + cleaningTimeForOneRoom
-    );
-  }
-
-  for (const room of roomsMap) {
-    const [, cleaningTimeForOneRoom] = room;
-    const totalCleaningTimeForOneCleaner = cleanerA.get(
-      TOTAL_CLEANING_TIME_FOR_ONE_CLEANER
-    );
-
-    const isCleaningTimeFitForOneCleaner =
-      totalCleaningTimeForOneCleaner < Math.floor(totalCleaningTime / 2) &&
-      totalCleaningTimeForOneCleaner + cleaningTimeForOneRoom <=
-        Math.floor(totalCleaningTime / 2);
-
-    if (isCleaningTimeFitForOneCleaner) {
-      updateCleaner(cleanerA, room);
-    } else if (!cleanerA.has(room)) {
-      updateCleaner(cleanerB, room);
+  roomsMap.forEach((cleaningTime, roomNumber) => {
+    // Check if the cleanerA map contains the room with the same room number
+    if (!cleanerA.has(roomNumber)) {
+      cleanerB.set(roomNumber, cleaningTime);
     }
-  }
+  });
+
   return [cleanerA, cleanerB];
 }
 
-console.log({ cleanerA, cleanerB });
+console.log({
+  cleanerA,
+  cleanerB,
+  totalCleaningTimeCleanerA,
+  totalCleaningTimeCleanerB,
+});
--- index.test.js
+++ index.test.js
@@ -1,8 +1,4 @@
-import {
-  setRoomsMap,
-  calculateTotalCleaningTime,
-  getRoomAssignments,
-} from './index';
+import { setRoomsMap, sumTotalCleaningTime, getRoomAssignments } from './index';
 
 const mockAvailableRooms = {
   101: 'DBS',
@@ -19,9 +15,9 @@ const mockAvailableRooms = {
   216: 'DDY',
 };
 
-describe('calculateTotalCleaningTime', () => {
+describe('sumTotalCleaningTime', () => {
   it('should calculate total cleaning time correctly', () => {
-    const totalCleaningTime = calculateTotalCleaningTime(
+    const totalCleaningTime = sumTotalCleaningTime(
       setRoomsMap(mockAvailableRooms)
     );
 
@@ -38,20 +34,7 @@ describe('getRoomAssignments', () => {
   it('should distribute all the rooms between two cleaners', () => {
     const roomsMap = setRoomsMap(mockAvailableRooms);
     const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
-
-    const cleanerArooms = ['101', '102', '103', '203', '207', '211', '215'];
-    const cleanerBrooms = ['210', '212', '213', '214', '216'];
-
-    expect(cleanerArooms.every((room) => cleanerA.has(room))).toBeTruthy();
-    expect(cleanerBrooms.every((room) => cleanerB.has(room))).toBeTruthy();
-
     const allRooms = [...roomsMap.keys()];
-
-    expect(
-      allRooms.every((room) =>
-        [...cleanerArooms, ...cleanerBrooms].includes(room)
-      )
-    ).toBeTruthy();
 
     expect(
       allRooms.every((room) => cleanerA.has(room) || cleanerB.has(room))
@@ -65,12 +48,24 @@ describe('getRoomAssignments', () => {
     const cleanerArooms = ['101', '102', '103', '203', '207', '211', '215'];
     const cleanerBrooms = ['210', '212', '213', '214', '216'];
 
-    expect(cleanerArooms.every((room) => !cleanerB.has(room))).toBeTruthy();
-    expect(cleanerBrooms.every((room) => !cleanerA.has(room))).toBeTruthy();
+    cleanerArooms.forEach((roomNumber) => {
+      expect(cleanerA.has(roomNumber)).toBeTruthy();
+      expect(cleanerB.has(roomNumber)).toBeFalsy();
+    });
+
+    cleanerBrooms.forEach((roomNumber) => {
+      expect(cleanerA.has(roomNumber)).toBeFalsy();
+      expect(cleanerB.has(roomNumber)).toBeTruthy();
+    });
+
+    // Check that there are no duplicate rooms
+    const allRooms = [...cleanerA.keys(), ...cleanerB.keys()];
+    const uniqueRooms = new Set(allRooms);
+    expect(uniqueRooms.size).toBe(allRooms.length);
   });
 
-  it('should handle an even-numbered totalCleaningTime', () => {
-    const roomsWithEvenNumberedTotalCleaningTime = {
+  it('should equally divide the room cleaningTimes if possible', () => {
+    const roomsWithEquallyDistributableCleaningTimes = {
       101: 'DBS',
       102: 'QDB',
       103: 'QDS',
@@ -84,14 +79,14 @@ describe('getRoomAssignments', () => {
       215: 'DDY',
     };
 
-    const roomsMap = setRoomsMap(roomsWithEvenNumberedTotalCleaningTime);
-    const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
-    const evenNumberedTotalCleaningTime = calculateTotalCleaningTime(roomsMap);
-
-    expect(evenNumberedTotalCleaningTime % 2 == 0).toBeTruthy();
-    expect(cleanerA.get('totalCleaningTimeForOneCleaner')).toEqual(
-      cleanerB.get('totalCleaningTimeForOneCleaner')
+    const [cleanerA, cleanerB] = getRoomAssignments(
+      setRoomsMap(roomsWithEquallyDistributableCleaningTimes)
     );
+
+    const totalCleaningTimeCleanerA = sumTotalCleaningTime(cleanerA);
+    const totalCleaningTimeCleanerB = sumTotalCleaningTime(cleanerB);
+
+    expect(totalCleaningTimeCleanerA).toEqual(totalCleaningTimeCleanerB);
   });
 
   it('should handle an odd-numbered totalCleaningTime', () => {
@@ -112,19 +107,46 @@ describe('getRoomAssignments', () => {
 
     const roomsMap = setRoomsMap(roomsWithOddNumberedTotalCleaningTime);
     const [cleanerA, cleanerB] = getRoomAssignments(roomsMap);
-    const oddNumberedTotalCleaningTime = calculateTotalCleaningTime(roomsMap);
+    const oddNumberedTotalCleaningTime = sumTotalCleaningTime(roomsMap);
+    const totalCleaningTimeCleanerA = sumTotalCleaningTime(cleanerA);
+    const totalCleaningTimeCleanerB = sumTotalCleaningTime(cleanerB);
 
     expect(oddNumberedTotalCleaningTime % 2 == 1).toBeTruthy();
-    expect(cleanerA.get('totalCleaningTimeForOneCleaner')).not.toEqual(
-      cleanerB.get('totalCleaningTimeForOneCleaner')
-    );
+    expect(totalCleaningTimeCleanerA).not.toEqual(totalCleaningTimeCleanerB);
   });
 
-  // 2. should only assign a room to one cleaner (no duplicate room assignments)
-  // 3. should handle an even-numbered totalCleaningTime
-  // 4. should handle an odd-numbered totalCleaningTime (e.g., one 15 min room)
-  // 5. if an equal cleaningTime is not possible between two cleaners, should never have a cleaningTime difference > 30 min
+  it('should minimize the cleaningTime difference between both cleaners if an equal cleaningTime is not possible between two cleaners', () => {
+    const roomsWithUnequallyDistributableCleaningTimes = {
+      101: 'ABS',
+      102: 'QDB',
+      103: 'QDS',
+      203: 'OC1',
+      207: 'DDY',
+      210: 'ODY',
+      211: 'DBS',
+      212: 'QDB',
+      213: 'QDS',
+      214: 'OC1',
+      215: 'DDY',
+      216: 'DDY',
+    };
 
-  // edgecase: 0 or 1 room should never happen for this usecase
-  // edgecase: 1 or 3 cleaner should never happen for this usecase
+    const [cleanerA, cleanerB] = getRoomAssignments(
+      setRoomsMap(roomsWithUnequallyDistributableCleaningTimes)
+    );
+
+    const totalCleaningTimeCleanerA = sumTotalCleaningTime(cleanerA);
+    const totalCleaningTimeCleanerB = sumTotalCleaningTime(cleanerB);
+
+    const timeDifference = Math.abs(
+      totalCleaningTimeCleanerA - totalCleaningTimeCleanerB
+    );
+
+    expect(timeDifference).toBeLessThanOrEqual(30);
+  });
+
+  // it('should maximize the number of rooms on the same floor for each cleaners (.e.g, cleanerA mostly has floor 1 rooms')
+
+  // edgecase: 0 or 1 room to clean should never happen for this usecase
+  // edgecase: 1 or 3+ cleaners should never happen for this usecase
 });
