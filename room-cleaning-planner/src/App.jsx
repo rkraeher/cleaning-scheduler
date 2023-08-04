1774bfdda4206ed88bd7ddb94907f57bc895a2c1
ru <rudikraeher@gmail.com>
2023-08-04 13:58:06 +0200

    add file upload input and map rows that contain roomNumbers onto a parsed json array

--- room-cleaning-planner/src/App.jsx
+++ room-cleaning-planner/src/App.jsx
@@ -2,77 +2,49 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { read, utils, writeFileXLSX } from 'xlsx';
 
 function FileUpload() {
-  const handleUpload = () => {
-    alert('File uploaded');
+  function isNumber(value) {
+    if (typeof value === 'string') {
+      return !isNaN(value);
+    }
+  }
+
+  const parseData = (data) => {
+    let parsedRows = [];
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i][0], isNumber(data[i][0]));
+      if (data[i][0] && isNumber(data[i][0])) {
+        parsedRows.push([...data[i]]);
+        i++;
+      }
+    }
+    console.log({ parsedRows });
+  };
+
+  const handleFile = async (e) => {
+    const file = e.target.files[0];
+    const data = await file.arrayBuffer();
+    // const workbook = read(data, { sheetRows: 6 });
+    const workbook = read(data);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = utils.sheet_to_json(worksheet, {
+      header: 1,
+      defval: '',
+    });
+
+    const parsedData = parseData(jsonData);
+    console.log({ jsonData, parsedData });
   };
   return (
-    <div className="App">
-      <form onSubmit={handleUpload}>
-        <h1>Upload a file</h1>
-        <input type="file" />
-        <button type="submit">Upload</button>
-      </form>
+    <div>
+      <input type="file" onChange={(e) => handleFile(e)} />
     </div>
   );
 }
 
 export default function App() {
-  /* the component state is an array of presidents */
-  const [pres, setPres] = useState([]);
-
-  /* Fetch and update the state once */
-  useEffect(() => {
-    (async () => {
-      const f = await // sample spreadsheet
-      (await fetch('https://sheetjs.com/pres.xlsx')).arrayBuffer();
-      const wb = read(f); // parse the array buffer
-      const ws = wb.Sheets[wb.SheetNames[0]]; // get the first worksheet
-      const data = utils.sheet_to_json(ws); // generate objects
-      setPres(data); // update state
-    })();
-  }, []);
-
-  /* get state data and export to XLSX */
-  const exportFile = useCallback(() => {
-    const ws = utils.json_to_sheet(pres);
-    const wb = utils.book_new();
-    utils.book_append_sheet(wb, ws, 'Data');
-    writeFileXLSX(wb, 'mySpreadSheet.xlsx');
-  }, [pres]);
-
-  async function handleFileAsync(e) {
-    const file = e.target.files[0];
-    const data = await file.arrayBuffer();
-    /* data is an ArrayBuffer */
-    const workbook = read(data);
-
-    /* DO SOMETHING WITH workbook HERE */
-  }
-
   return (
     <div>
       <h1>Room Cleaning Planner</h1>
-      <table>
-        <thead>
-          <th>Name</th>
-          <th>Index</th>
-        </thead>
-        <tbody>
-          {
-            /* generate row for each president */
-            pres.map((pres) => (
-              <tr>
-                <td>{pres.Name}</td>
-                <td>{pres.Index}</td>
-              </tr>
-            ))
-          }
-        </tbody>
-        <td colSpan={2}>
-          <button onClick={exportFile}>Export XLSX</button>
-        </td>
-      </table>
-      <br />
       <FileUpload />
     </div>
   );
