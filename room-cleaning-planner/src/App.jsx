1710c7a5e35a525695d8990bde92db798008476d
ru <rudikraeher@gmail.com>
2023-08-05 13:19:55 +0200

    add tests for input parsing helper functions

--- room-cleaning-planner/src/App.jsx
+++ room-cleaning-planner/src/App.jsx
@@ -1,51 +1,56 @@
 import React from 'react';
 import { read, utils } from 'xlsx';
 
+// isNumberAsString?
+export function isNumber(value) {
+  if (typeof value === 'string' && value.trim() !== '') {
+    return !Number.isNaN(Number(value));
+  }
+  return false;
+}
+
+export function parseRow(row) {
+  // both these condition checks are highly specific to the usecase
+  // we could combine them into a single condition, isRowRelevant
+  function isTimeCode(cell) {
+    // matches D, Q, or O followed by either two capital letters or a capital letter and a number
+    const regex = /^(D|Q|O)([A-Z]{2}|[A-Z]\d)$/;
+    return regex.test(cell);
+  }
+
+  function isAvailability(cell) {
+    // matches for available or till mm.dd. permits single or double digit day.month as well
+    const regex = /^(available|till \d{1,2}\.\d{1,2})$/;
+    return regex.test(cell);
+  }
+
+  let parsedRow = [];
+  for (let i = 0; i < row.length; i++) {
+    if (
+      (row[i] && isNumber(row[i])) ||
+      isTimeCode(row[i]) ||
+      isAvailability(row[i])
+    ) {
+      parsedRow.push(row[i]);
+    }
+  }
+  return parsedRow;
+}
+
+export function parseRows(data) {
+  let parsedRows = [];
+  // for later: use filter() instead of this loop
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] && isNumber(data[i][0])) {
+      let parsedRow = parseRow(data[i]);
+      parsedRows.push([...parsedRow]);
+    }
+  }
+  return parsedRows;
+}
+
+// make FileUpload a separate component
 function FileUpload() {
-  function isNumber(value) {
-    if (typeof value === 'string') {
-      return !Number.isNaN(Number(value));
-    }
-  }
-
-  const parseRow = (row) => {
-    // both these condition checks are highly specific to the usecase
-    function isTimeCode(cell) {
-      // matches D, Q, or O followed by either two capital letters or a capital letter and a number
-      const regex = /^(D|Q|O)([A-Z]{2}|[A-Z]\d)$/;
-      return regex.test(cell);
-    }
-
-    function isAvailability(cell) {
-      // matches for available or till mm.dd. permits single or double digit day.month as well
-      const regex = /^(available|till \d{1,2}\.\d{1,2})$/;
-      return regex.test(cell);
-    }
-
-    let parsedRow = [];
-    for (let i = 0; i < row.length; i++) {
-      if (
-        (row[i] && isNumber(row[i])) ||
-        isTimeCode(row[i]) ||
-        isAvailability(row[i])
-      ) {
-        parsedRow.push(row[i]);
-      }
-    }
-    return parsedRow;
-  };
-
-  function parseRows(data) {
-    let parsedRows = [];
-    for (let i = 0; i < data.length; i++) {
-      if (data[i][0] && isNumber(data[i][0])) {
-        let parsedRow = parseRow(data[i]);
-        parsedRows.push([...parsedRow]);
-      }
-    }
-    return parsedRows;
-  }
-
   const handleFile = async (e) => {
     const file = e.target.files[0];
     const data = await file.arrayBuffer();
