514e8a8d12d654f3cca4f418aed6da759dfe399a
rudi kraeher <rudikraeher@gmail.com>
2023-10-14 11:40:58 +0200

    Implement logic to calculate cleaningTime sums for stays and departures for both lists; in this commit it is done in parallel to existing implementation to keep the logic and tests running, so this is a transitional commit before full implementation replacement

--- src/getBalancedRoomLists.js
+++ src/getBalancedRoomLists.js
@@ -5,13 +5,14 @@ import {
 } from './constants';
 
 export function getBalancedRoomLists(rooms) {
-  const roomsMap = mapRoomsToCleaningTimes(rooms);
+  const { roomsMap, roomsMap2 } = mapRoomsToCleaningTimes(rooms);
 
-  return distributeRooms(roomsMap);
+  return distributeRooms(roomsMap, roomsMap2);
 }
 
 export function mapRoomsToCleaningTimes(rooms) {
   let roomsMap = new Map();
+  const roomsMap2 = {};
 
   for (const [roomNumber, cleaningTimeCode, , , roomState] of rooms) {
     // only the first letter (index 0) from the timeCodes cell is needed to set the departure cleaningTime for a room
@@ -21,17 +22,28 @@ export function mapRoomsToCleaningTimes(rooms) {
         ? CLEANING_TIMES_IN_MINUTES[departure]
         : CLEANING_TIMES_IN_MINUTES[roomState];
 
+    // add an additional value here, roomState; rename the mapping fn
+    roomsMap2[roomNumber] = { cleaningTime, roomState };
     roomsMap.set(roomNumber, cleaningTime);
   }
-  return roomsMap;
+  return { roomsMap, roomsMap2 };
 }
 
-function distributeRooms(roomsMap) {
+function distributeRooms(roomsMap, roomsMap2) {
   const roomsListA = new Map();
   const roomsListB = new Map();
 
+  const roomsListA2 = new Map();
+  const roomsListB2 = new Map();
+  // why we need this separate map instead of just roomsLists?
+  const staysAndDepsSumA = new Map();
+  const staysAndDepsSumB = new Map();
+
   const halfTotalCleaningTime = getRoundedHalfCleaningTime(
     sumCleaningTime(roomsMap)
+  );
+  const halfTotalCleaningTime2 = getRoundedHalfCleaningTime(
+    sumCleaningTime2(roomsMap2)
   );
 
   for (const [room, time] of roomsMap) {
@@ -40,8 +52,35 @@ function distributeRooms(roomsMap) {
       : roomsListB.set(room, time);
   }
 
+  for (const room in roomsMap2) {
+    const cleaningTime = roomsMap2[room]['cleaningTime'];
+    const roomState = roomsMap2[room]['roomState'];
+
+    if (sumCleaningTime(roomsListA2) + cleaningTime <= halfTotalCleaningTime2) {
+      roomsListA2.set(room, cleaningTime);
+      calculateStaysAndDeps(cleaningTime, roomState, staysAndDepsSumA);
+    } else {
+      roomsListB2.set(room, cleaningTime);
+      calculateStaysAndDeps(cleaningTime, roomState, staysAndDepsSumB);
+    }
+  }
+
+  // we can also setRoomsListX.set(room, roomsData.time), inside this function?
+  function calculateStaysAndDeps(time, state, staysAndDepsSum) {
+    if (state === ROOM_STATES.STAY) {
+      const currentSum = staysAndDepsSum.get('Stays') || 0;
+      staysAndDepsSum.set('Stays', currentSum + time);
+    }
+    if (state === ROOM_STATES.DEPARTURE) {
+      const currentSum = staysAndDepsSum.get('Departures') || 0;
+      staysAndDepsSum.set('Departures', currentSum + time);
+    }
+  }
+
   roomsListA.set(TOTAL_CLEANING_TIME, sumCleaningTime(roomsListA));
   roomsListB.set(TOTAL_CLEANING_TIME, sumCleaningTime(roomsListB));
+
+  console.log({ roomsListA2, staysAndDepsSumA, roomsListB2, staysAndDepsSumB });
 
   return { roomsListA, roomsListB };
 
@@ -53,6 +92,10 @@ function distributeRooms(roomsMap) {
 
     return roundedHalfCleaningTime;
   }
+}
+
+function sumCleaningTime2(rooms) {
+  return Object.values(rooms).reduce((sum, room) => sum + room.cleaningTime, 0);
 }
 
 export function sumCleaningTime(rooms) {
--- src/getBalancedRoomLists.test.js
+++ src/getBalancedRoomLists.test.js
@@ -42,7 +42,7 @@ const rooms = [
 
 describe('sumCleaningTime', () => {
   it('should calculate the sum cleaningTime for a list of rooms', () => {
-    const roomsMap = mapRoomsToCleaningTimes(rooms);
+    const { roomsMap, roomsMap2 } = mapRoomsToCleaningTimes(rooms);
     const result = sumCleaningTime(roomsMap);
     const expectedSum = 1350;
 
